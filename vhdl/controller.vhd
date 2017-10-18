ca7fe7f9a8cb37341ffbe1a785d126ea96a5fda7
Anton Ragot <anton.ragot@epfl.ch>
2017-10-18 11:57:41 +0200

    changement controller

--- vhdl/controller.vhd
+++ vhdl/controller.vhd
@@ -38,7 +38,7 @@ end controller;
 
 architecture synth of controller is
 
-type op_state is (FETCH1, FETCH2, DECODE, R_OP, STORE, BREAK, LOAD1, LOAD2, I_OP);
+type op_state is (FETCH1, FETCH2, DECODE, R_OP, STORE, BREAK, LOAD1, LOAD2, I_OP, BRANCH, CALL, JMP);
 signal state, next_state : op_state;
 
 begin
@@ -81,11 +81,14 @@ begin
 		when DECODE => if("00"&op = X"3A") then
 					if("00"&opx = X"34") then
 						 next_state <= BREAK;
+						elsif(("00"&opx = X"05") or ("00"&opx = X"0D")) then next_state <= JMP;
 						else next_state <= R_OP;
 					end if;
 			       elsif("00"&op = X"04") then next_state <= I_OP;
 			       elsif("00"&op = X"17") then next_state <= LOAD1;
 			       elsif("00"&op = X"15") then next_state <= STORE;
+			       elsif(("00"&op = X"06") or ("00"&op = X"0E") or ("00"&op = X"16") or ("00"&op = X"1E") or ("00"&op = X"26") or ("00"&op = X"2E") or ("00"&op = X"36")) then next_state <= BRANCH;
+			       elsif("00"&op = X"00") then next_state <= CALL;	
 			end if;
 		when I_OP => next_state <= FETCH1;
 				rf_wren <= '1';
@@ -109,13 +112,41 @@ begin
 				sel_mem <= '1';
 				rf_wren <= '1';
 				-- Pas besoin de changer sel_rc
+		when BRANCH =>  next_state <= FETCH1;
+				sel_b <= '1';
+				branch_op <= '1';
+				pc_add_imm <= '1';
+		when CALL => next_state <= FETCH1;
+			     sel_ra <= '1';
+			     -- sel_rc reste a 0
+			     --sel_mem reste a 0
+			     sel_pc <= '1';
+			     rf_wren <= '1';
+		when JMP => next_state <= FETCH1;
+			    pc_en <= '1';
+			    pc_sel_a <= '1';
 
 	end case;
 end process;
 
 op_signal: process(op, opx)
 begin
-
+	if("00"&op = X"3A") then
+		op_alu(2 downto 0) <= opx(5 downto 3);
+		case "00"&opx is 
+			when X"0E" => op_alu(5 downto 3) <= "10-";
+			when X"1B" => op_alu(5 downto 3) <= "11-";
+			when others => op_alu(5 downto 3) <= "---";
+		end case;
+	else
+		op_alu(2 downto 0) <= op(5 downto 3);
+		case "00"&op is
+			when X"04" | X"17" | X"15" => op_alu(5 downto 3) <= "000";
+			when X"06" => op_alu <= "011100";
+			when X"0E" | X"16" | X"1E" | X"26" | X"2E" | X"36" => op_alu(5 downto 3) <= "011";
+			when others => op_alu(5 downto 3) <= "---";
+		end case;
+	end if;
 end process;
 
 end synth;
